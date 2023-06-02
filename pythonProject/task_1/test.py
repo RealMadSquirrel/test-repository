8b8da8462842811495e13bbc61f223a96beb67cd
RealMadSquirrel <nikolaevaolga1993@gmail.com>
2023-06-02 15:22:12 +0300

    Исправление инициализации браузера

--- pythonProject/task_1/test.py
+++ pythonProject/task_1/test.py
@@ -1,18 +1,12 @@
 import unittest
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 
 
 print("Выполнить один раз перед выполнением всех тестов")
 
 options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors')
 driver = webdriver.Chrome(options=options)
-s = Service('C:/Users/user/PycharmProjects/pythonFirst/chromedriver.exe')
-driver.implicitly_wait(10)
-driver = webdriver.Chrome(service=s)
 driver.maximize_window()
 driver.get("https://www.gloria-jeans.ru/")
 time.sleep(6)
